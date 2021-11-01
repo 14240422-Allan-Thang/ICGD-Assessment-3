29bc2dfa941819273cf7a47569ac487a815fbefc
14240422-Allan-Thang <Allan.Thang@student.uts.edu.au>
2021-11-02 01:13:45 +1100

    Finished Band

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    Button exitButton;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +19,35 @@ public class UIManager : MonoBehaviour
         
     }
 
+    public void LoadTitle()
+    {
+        GameManager.CurrentGameState = GameManager.GameState.Title;
+        SceneManager.LoadSceneAsync(0);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
     public void LoadLevel1()
     {
         GameManager.CurrentGameState = GameManager.GameState.Level1;
         SceneManager.LoadSceneAsync(1);
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
     public void LoadLevel2()
     {
         GameManager.CurrentGameState = GameManager.GameState.Level2;
         SceneManager.LoadSceneAsync(2);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.buildIndex == 1)
+        {
+            exitButton = GameObject.FindGameObjectWithTag("ExitButton").GetComponent<Button>();
+            exitButton.onClick.AddListener(LoadTitle);
+        }
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
     private void Awake()
